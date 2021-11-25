c5d0df9b39d2bd9dea7774bd86f5233900d333bd
Git Man <ap4mobile@gmail.com>
2021-11-25 23:07:22 +0500

    MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,27 +1,35 @@
 import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-const API_URL = 'https://course-vue.javascript.ru/api';
-
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
-
 // Требуется создать Vue приложение
 const RootComponent = defineComponent({
 	name: 'Root',
 	data(){
 		return {
-      filter: '',
+      API_URL: 'https://course-vue.javascript.ru/api',
+      meetupId: '0',
+      meetupObject: {
+        title: 'No title'
+      },
 		};
 	},
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${this.API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    },
+  },
+  watch: {
+    async 'meetupId'() {
+      this.meetupObject = await this.fetchMeetupById(this.meetupId);
+    },
+  },
 });
 
 const app = createApp(RootComponent);
