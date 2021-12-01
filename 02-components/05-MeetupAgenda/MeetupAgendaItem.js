45cc9290c06355446297c8e3f49d6e7b5f81e32f
Git Man <ap4mobile@gmail.com>
2021-12-01 12:59:31 +0500

    MeetupCover && MeetupAgenda - Исправление ошибок 2

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -25,7 +25,7 @@ export default defineComponent({
       <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
         <h3 class="agenda-item__title">{{ agendaItemTitle }}</h3>
-        <p class="agenda-item__talk" v-show="agendaItem.speaker != null">
+        <p class="agenda-item__talk" v-if="agendaItem.type == 'talk'">
           <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
           <span class="agenda-item__lang">{{ agendaItem.language }}</span>
