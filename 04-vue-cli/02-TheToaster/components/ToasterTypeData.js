b0e47dec41620b5e27492ca80a1e016dde2ebafd
Git Man <ap4mobile@gmail.com>
2021-12-20 23:07:40 +0500

    TheToaster

---
+++ 04-vue-cli/02-TheToaster/components/ToasterTypeData.js
@@ -0,0 +1,9 @@
+export const toasterClasses = {
+    success: 'toast_success',
+    error: 'toast_error',
+};
+
+export const toasterIcons = {
+    success: 'check-circle',
+    error: 'alert-circle'
+};
